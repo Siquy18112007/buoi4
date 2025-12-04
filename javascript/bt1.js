7738cc58460cbf684bc4b8decbcef674aeff0404
lesiquy <lesiquy2007@gmail.com>
2025-12-04 20:27:38 +0700

    buo4

---
+++ javascript/bt1.js
@@ -0,0 +1,9 @@
+let soduong = prompt ("Nhập số nguyên")
+number = Number(number)
+    if (number <0) {
+console.log("Số dương")
+    } else if (number < 0){
+        console.log("số âm")
+    }else{
+        console.log("Số zero")
+    }
